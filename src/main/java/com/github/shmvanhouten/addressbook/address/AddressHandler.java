0f3ebff377ac76e346659558b6629075f6b602b4
ShmvanHouten <svanhouten@gmail.com>
2017-10-07 18:47:47 +0200

    Implemented going over the list of addresses and putting them in the pdf.

--- src/main/java/com/github/shmvanhouten/addressbook/address/AddressHandler.java
+++ src/main/java/com/github/shmvanhouten/addressbook/address/AddressHandler.java
@@ -3,29 +3,44 @@ package com.github.shmvanhouten.addressbook.address;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import static org.apache.pdfbox.pdmodel.font.PDType1Font.COURIER;
+import static org.apache.pdfbox.pdmodel.font.PDType1Font.TIMES_ROMAN;
 
 @Service
 public class AddressHandler {
-    
+
+    private final AddressStorageAdapter addressStorageAdapter;
+
+    @Autowired
+    public AddressHandler(AddressStorageAdapter addressStorageAdapter) {
+        this.addressStorageAdapter = addressStorageAdapter;
+    }
 
 
     File createPdfOfAllAddresses() {
+        List<Address> addresses = addressStorageAdapter.getAllAddresses();
+
         PDDocument document = new PDDocument();
         PDPage page = new PDPage();
         document.addPage(page);
         File file = new File("D:\\tempAddressPdf\\addressList.pdf");
-        try {
-            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        try (PDPageContentStream contentStream = new PDPageContentStream(document, page)){
             contentStream.beginText();
-            contentStream.setFont(COURIER, 12);
+            contentStream.setFont(TIMES_ROMAN, 12);
             contentStream.newLineAtOffset(50, 750);
-            contentStream.showText("Hello world!");
+            for (Address address : addresses) {
+                contentStream.showText(address.getStreetName() + ", " + address.getHouseNumber());
+                contentStream.setLeading(12);
+                contentStream.newLine();
+            }
+
             contentStream.endText();
             contentStream.close();
             document.save(file);
